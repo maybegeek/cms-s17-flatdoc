2dc4f1f04b105c259bc6061754c12b833e2381ba
maybegeek <chr.pfeiffer@gmail.com>
2017-04-15 13:51:00 +0200

    waypoint files and settings for waypoints in the custom.js

--- js/flatdoc-custom.js
+++ js/flatdoc-custom.js
@@ -10,5 +10,66 @@ $(document).on('flatdoc:ready', function() {
   wrapYamlAndContent.date === 0 ? '' : $("#flat-date").html( jQuery.timeago( wrapYamlAndContent.date ) ) ;
   wrapYamlAndContent.date === 0 ? '' : $("#flat-date").attr( 'datetime', wrapYamlAndContent.date ) ;
 
+  // sticky sidebar
+  var sticky = new Waypoint.Sticky({
+    element: $('#flatdoc-menu')[0]
+  })
+
+  // wrap section-tag around heading-element till before next heading
+  // with section-id (plus suffix) from heading-id
+  $(function () {
+      $('main h1,main h2,main h3').each(function () {
+          var headingId =  $(this).attr('id');
+          var sectionId = headingId+'-wayp-section-container';
+          $(this).nextUntil('main h1, main h2, main h3').add(this).wrapAll('<section id="'+sectionId+'" />');
+      });
+  });
+
+  $(document).ready(function() {
+
+    // Get section or article by href
+    function getRelatedContent(el){
+      return $($(el).attr('href'));
+    }
+    // smooth scrolling
+    $('nav a').on('click',function(e){
+      e.preventDefault();
+      $('html,body').animate({scrollTop:getRelatedContent(this).offset().top - 20})
+    });
+
+    // the waypoint thing!
+    $('main section').each(function() {
+    var waypSectionId;
+    var inview = new Waypoint.Inview({
+      element: this,
+      enter: function(direction) {
+        // transform section-id to corresponding nav a href
+        waypSectionId = $(this.element).attr('id').replace('-wayp-section-container','');
+        $(this.element).toggleClass('inview');
+        $('nav a[href=#'+waypSectionId+']').toggleClass('active');
+        //console.log('Richtung: '+direction+' \nStatus: enter \nID: '+$(this.element).attr('id'));
+      },
+      entered: function(direction) {
+        // transform section-id to corresponding nav a href
+        waypSectionId = $(this.element).attr('id').replace('-wayp-section-container','');
+        //console.log('Richtung: '+direction+' \nStatus: entered \nID: '+$(this.element).attr('id'));
+      },
+      exit: function(direction) {
+        // transform section-id to corresponding nav a href
+        waypSectionId = $(this.element).attr('id').replace('-wayp-section-container','');
+        //console.log('Richtung: '+direction+' \nStatus: exit \nID: '+$(this.element).attr('id'));
+      },
+      exited: function(direction) {
+        // transform section-id to corresponding nav a href
+        waypSectionId = $(this.element).attr('id').replace('-wayp-section-container','');
+        $(this.element).toggleClass('inview');
+        $('nav a[href=#'+waypSectionId+']').toggleClass('active');
+        //console.log('Richtung: '+direction+' \nStatus: exited \nID: '+$(this.element).attr('id'));
+      }
+    })
+    })
+
+  }); // end document:ready
+
 }); // end flatdoc:ready
 
